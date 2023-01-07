d73cd489316be9c22171a2c95d27a0e16faf172b
tarasinghrajput <TARASINGHRAJPUT7261@GMAIL.COM>
2023-01-07 18:32:48 +0530

    aimimg mechanics implemented

--- ZombieShooterGame/Assets/Scripts/Player.cs
+++ ZombieShooterGame/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     public FixedJoystick moveJoystick;
     public FixedJoystick aimJoystick;
     Vector3 moveVelocity;
+    Vector3 aimVelocity;
     public Rigidbody2D rb;
     public float moveSpeed;
     public float fireSpeed;
@@ -25,5 +26,17 @@ public class Player : MonoBehaviour
         Vector2 moveInput = new Vector2(moveVelocity.x, moveVelocity.y);
         Vector2 moveDir = moveInput.normalized * moveSpeed;
         rb.MovePosition(rb.position + moveDir * Time.deltaTime);
+
+
+        // aimVelocity = new Vector3(aimJoystick.Horizontal, aimJoystick.Vertical);
+        // Vector3 aimInput = new Vector3(aimVelocity.x, aimVelocity.y)* Mathf.Rad2Deg;;
+        // Vector3 lookAtPoint = transform.position + aimInput;
+        // transform.LookAt(lookAtPoint);
+
+        Vector3 moveVector = (Vector3.up * aimJoystick.Horizontal + Vector3.left * aimJoystick.Vertical);
+        if (aimJoystick.Horizontal != 0 || aimJoystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, moveVector);
+        }
     }
 }
